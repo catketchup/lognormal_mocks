08fd12a0a0f58ca1a3ee68a96340f79a0d18d282
ketchup <hoc34@pitt.edu>
2023-03-05 02:23:42 -0500

    figured part of paths

--- python/setup.py
+++ python/setup.py
@@ -2,22 +2,32 @@ from distutils.core import setup
 from distutils.extension import Extension
 from distutils.sysconfig import get_python_lib
 from Cython.Build import cythonize
-import numpy
 import os
-import subprocess as sbp
-import os.path as osp
 
 numpy_inc = os.path.join(get_python_lib(plat_specific=1), 'numpy/core/include')
 
+# include paths
+healpix_include_path = '/home/ketchup/tools/Healpix_3.82_2022Jul28/Healpix_3.82/include'
+lapack_include_path = '/home/ketchup/tools/lapack-3.11.0'
+sharp_include_path = '/home/ketchup/tools/libsharp/auto/include'
+lognormal_mocks_include_path = '/home/ketchup/tools/lognormal_mocks/lognormal_mocks/include'
+
+# lib paths
+liblognormal_mocks_path = '/home/ketchup/tools/lognormal_mocks/build/lib'
+libhealpix_path = '/home/ketchup/tools/Healpix_3.82_2022Jul28/Healpix_3.82/lib'
+liblapack_path = '/home/ketchup/tools/lapack-3.11.0'
+libsharp_path = '/home/ketchup/tools/libsharp/auto/lib'
+libmvec_path = '/lib/x86_64-linux-gnu'
+
 lognormal_mocks_extension = Extension(name='py_lognormal_mocks',
                                       sources=['python/py_lognormal_mocks.pyx'],
-                                      include_dirs = [numpy_inc,'/home/ketchup/tools/lognormal_mocks/lognormal_mocks/include', '/home/ketchup/tools/Healpix_3.82_2022Jul28/Healpix_3.82/include','/home/ketchup/tools/lapack-3.11.0','/home/ketchup/tools/libsharp/auto/include'],
-                                    libraries=['/home/ketchup/tools/lognormal_mocks/build/lib/lognormal_mocks', 'gsl','gslcblas','/home/ketchup/tools/Healpix_3.82_2022Jul28/Healpix_3.82/lib/healpix','cfitsio','gomp','/home/ketchup/tools/lapack-3.11.0/lapack','/home/ketchup/tools/libsharp/auto/lib/sharp', '/home/ketchup/tools/libsharp/auto/lib/fftpack', '/home/ketchup/tools/libsharp/auto/lib/c_utils', 'gfortran', 'gcc', '/lib/x86_64-linux-gnu/mvec'])
+                                      include_dirs = [numpy_inc, lognormal_mocks_include_path, healpix_include_path, lapack_include_path, sharp_include_path],
+                                      libraries=['lognormal_mocks', 'gsl','gslcblas', 'healpix','cfitsio','gomp', 'lapack','sharp', 'fftpack', 'c_utils', 'gfortran', 'mvec'],
+                                      library_dirs = [liblognormal_mocks_path, libhealpix_path, liblapack_path, libsharp_path, libmvec_path],
+                                      extra_compile_args=['-fPIC','-Wall','-g']
+)
 
-setup(name='py_lognormal_mocks',
+setup(author='Hongbo Cai',
+      author_email='hoc34@pitt.edu',
+      name='py_lognormal_mocks',
       ext_modules=cythonize([lognormal_mocks_extension]))
-
-# setup(author='Hongbo Cai',
-#       author_email='hoc34@pitt.edu',
-#       name='pytoy',
-#       ext_modules=cythonize([toy_extension]))
