fbe597376cf8550193fb2e7d0c3bb434605db715
ketchup <hoc34@pitt.edu>
2023-03-04 23:29:10 -0500

    success! to adjust paths

--- python/py_lognormal_mocks.pyx
+++ python/py_lognormal_mocks.pyx
@@ -5,6 +5,8 @@ from libc.stdio cimport *
 from libc.string cimport *
 import cython
 cimport cython
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+
 
 cdef extern from "lognormal_mocks.h":
     void lognormal_mocks_stats_fullsky(
@@ -18,11 +20,11 @@ cdef extern from "lognormal_mocks.h":
 
 def lognormal_mocks_stats(double[:] rhobar, double[:,:,::1] Cl, Ntheta):
     Nmaps = rhobar.size
-    Nl = Cl.shape[-1]
+    Nl = Cl.shape[2]
 
     cdef np.ndarray[np.float64_t, ndim=1] gaussbar = np.zeros(Nmaps) # output
     cdef np.ndarray[np.float64_t, ndim=3] Clgauss = np.zeros([Nmaps, Nmaps, Nl]) # output
-    cdef np.ndarray[np.float64_t, ndim=3] xidelta = np.zeros([Nmaps, Nmaps, Ntheta])
+    cdef np.ndarray[np.float64_t, ndim=3] xidelta = np.zeros([Nmaps, Nmaps, Ntheta]) # input
 
     lognormal_mocks_stats_fullsky(Nmaps, &rhobar[0], Nl, &Cl[0,0,0], &gaussbar[0], &Clgauss[0,0,0], &xidelta[0,0,0], Ntheta)
 
