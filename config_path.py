ebd53442225e6276cf598bb7f5fd4b6e513b8396
ketchup <hoc34@pitt.edu>
2023-03-08 14:28:23 -0500

    added config_path.py

---
+++ config_path.py
@@ -0,0 +1,21 @@
+# gsl
+gsl_include_path =
+gsl_lib_path =
+
+# healpix
+healpix_include_path =
+healpix_lib_path =
+
+# lapack
+lapack_include_path =
+lapack_lib_path =
+
+# sharp
+sharp_include_path =
+# libc_utils.a, libfftpack.a and libsharp.a should be in this dir
+sharp_lib_path =
+
+# glibc
+glibc_include_path =
+# libmvec.a should be in this dir
+glibc_lib_path = 
--- python/setup.py
+++ python/setup.py
@@ -7,30 +7,18 @@ import os, sys, inspect
 numpy_inc = os.path.join(get_python_lib(plat_specific=1), 'numpy/core/include')
 current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_path = os.path.dirname(current_path)
+sys.path.insert(0, parent_path)
 
-# dependence paths from input
-healpix_path = input("Enter healpix path:")
-lapack_path = input("Enter lapack path:")
-sharp_path = input("Enter libsharp path:")
+import config_path as cp
 
-# include paths
-healpix_include_path = healpix_path + '/include'
-lapack_include_path = lapack_path
-sharp_include_path = sharp_path + '/auto/include'
 lognormal_mocks_include_path = parent_path + '/lognormal_mocks/include'
-
-# library paths
-libhealpix_path = healpix_path + '/lib'
-liblapack_path = lapack_path
-libsharp_path = sharp_path + '/auto/lib'
-libmvec_path = '/lib/x86_64-linux-gnu'
-liblognormal_mocks_path = parent_path + '/build/lib'
+lognormal_mocks_lib_path = parent_path + '/build/lib'
 
 lognormal_mocks_extension = Extension(name='py_lognormal_mocks',
                                       sources=['python/py_lognormal_mocks.pyx'],
-                                      include_dirs = [numpy_inc, lognormal_mocks_include_path, healpix_include_path, lapack_include_path, sharp_include_path],
+                                      include_dirs = [numpy_inc, lognormal_mocks_include_path, cp.healpix_include_path, cp.lapack_include_path, cp.sharp_include_path],
                                       libraries=['lognormal_mocks', 'gsl','gslcblas', 'healpix','cfitsio','gomp', 'lapack','sharp', 'fftpack', 'c_utils', 'gfortran', 'mvec'],
-                                      library_dirs = [liblognormal_mocks_path, libhealpix_path, liblapack_path, libsharp_path, libmvec_path],
+                                      library_dirs = [lognormal_mocks_lib_path, cp.healpix_lib_path, cp.lapack_lib_path, cp.sharp_lib_path, cp.glibc_lib_path],
                                       extra_compile_args=['-fPIC','-Wall','-g']
 )
 
